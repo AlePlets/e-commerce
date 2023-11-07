a1d4702504ade11458e0c61c236ec9bcf7069ba6
Alessandra Regina da Silva <alessandraregina90@gmail.com>
2023-11-07 20:20:17 -0300

    teste no .env

--- projeto/src/server.js
+++ projeto/src/server.js
@@ -7,12 +7,13 @@ import { errorMiddleware } from './middlewares/errorMiddleware.js';
 
 const app = express();
 
-dotenv.config();
+dotenv.config({path: '../.env'});
+
+const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT || 3000;
 
 
-const MONGODB_URI = process.env.MONGODB_URI 
-
-mongoose.connect("mongodb+srv://ale:ale@cluster0.dalhrji.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
@@ -20,11 +21,9 @@ db.once('open', () => {
 });
 
 app.use(express.json());
-
 app.use(productRoutes);
 app.use(cartRoutes);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server está rodando em: http://localhost:${PORT}`);
 });
